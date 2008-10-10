f0c145194a21f3db7a8bad8f79f41ca7d8133782
Nirav Thaker <nirav.thaker@gmail.com>
2008-10-10 15:48:18 +0000

    Added for new feature command history

---
+++ net.sf.pyconsole/test/net/sf/pyconsole/LIFOQueueTest.java
@@ -0,0 +1,42 @@
+package net.sf.pyconsole;
+
+import junit.framework.TestCase;
+
+public class LIFOQueueTest extends TestCase {
+
+	public void testNext() {
+		LIFOQueue<String> queue = new LIFOQueue<String>();
+		queue.push("Abc");
+		queue.push("Pqr");
+		queue.push("Xyz");
+		assertEquals("Xyz", queue.next());
+		assertEquals("Pqr", queue.next());
+		assertEquals("Abc", queue.next());
+
+		assertEquals("Xyz", queue.next());
+		assertEquals("Pqr", queue.next());
+		assertEquals("Abc", queue.next());
+
+		assertEquals("Xyz", queue.next());
+		assertEquals("Pqr", queue.next());
+		assertEquals("Abc", queue.next());
+	}
+
+	public void testPrevious() {
+		LIFOQueue<String> queue = new LIFOQueue<String>();
+		queue.push("Abc");
+		queue.push("Pqr");
+		queue.push("Xyz");
+		assertEquals("Abc", queue.previous());
+		assertEquals("Pqr", queue.previous());
+		assertEquals("Xyz", queue.previous());
+
+		assertEquals("Pqr", queue.next());
+		assertEquals("Abc", queue.next());
+		assertEquals("Xyz", queue.next());
+
+		assertEquals("Abc", queue.previous());
+		assertEquals("Pqr", queue.previous());
+		assertEquals("Abc", queue.next());
+	}
+}
